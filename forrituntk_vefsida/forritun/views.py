6cf6c23280630a0b9b386a8f03088f17d2916fa3
Arnór Valdimarsson <arnorv@gmail.com>
2014-12-22 19:59:25 +0000

    Basic authentication kerfi

--- forrituntk_vefsida/forritun/views.py
+++ forrituntk_vefsida/forritun/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.contrib import auth
+from django.template import RequestContext
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated():
+        return render(request, 'skradurInn.html')
+    else:
+        return render(request, 'index.html')
 
+
+def login_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        # Tókst að skrá inn
+        auth.login(request, user)
+        return HttpResponseRedirect("/")
+    else:
+        # Tekst ekki að skrá inn
+        return HttpResponseRedirect("/")
+
+
+def logout_view(request):
+    auth.logout(request)
+
+    return HttpResponseRedirect("/")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/accounts/login")
+    else:
+        form = UserCreationForm()
+    c = {'form': form}
+    return render_to_response("registration/register.html", c, context_instance=RequestContext(request))
--- forrituntk_vefsida/forrituntk/urls.py
+++ forrituntk_vefsida/forrituntk/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'forrituntk.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', 'forritun.views.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$',  login),
+    url(r'^accounts/logout/$',  logout),
+    url(r'^accounts/register/$',  'forritun.views.register'),
 )
