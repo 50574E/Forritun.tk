233612bb330372727a3f1409cee01de9a0b64b01
bjarkihall <bjarkihall@hotmail.com>
2015-01-02 19:56:52 +0000

    allskonar fikt, html, css, js, jq
    reynt að skipta upp templates betur og íslenska, lítill takki í hægra
    horni sem skiptir um lit (auðveldlega hægt að taka út), jquery bætt við,
    umfangsmeira css (auðveldlega hægt að aðlaga eftir þörfum til að fá
    æskilegra lúkk á síðuna)

---
+++ forrituntk_vefsida/forritun/static/bgColor.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	var isRed = false;
+	var c1 = "#2371e5";
+	var c2 = "#df0000";
+	$(".bgColor").click(function(){
+		isRed = !isRed;
+		if(isRed){
+			$("body").css("background",c2);
+			$("a").css("color",c2);
+			$(".bgColor").css("background",c1);
+		}else{
+			$("body").css("background",c1);
+			$("a").css("color",c1);
+			$(".bgColor").css("background",c2);
+		}
+	});
+
+	var colorHover = function(sel,bg,over){
+		var a = c1; var b = c2;
+		if(!over){var t = a; a = b; b = t;}
+		if(isRed) $(sel).css(bg,a);
+		else $(sel).css(bg,b);
+	};
+
+	$(".bgColor").mouseover(function(){colorHover(this,"background",true);});
+	$("a").mouseover(function(){colorHover(this,"color",true);});
+	$(".bgColor").mouseout(function(){colorHover(this,"background",false);});
+	$("a").mouseout(function(){colorHover(this,"color",false);});
+
+});
