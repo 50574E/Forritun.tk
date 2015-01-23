5e03889607429006be8beb38a5efdb43aef1b9d0
Arnór Valdimarsson <arnorv@gmail.com>
2015-01-23 20:05:17 +0000

    setti static_root

--- forrituntk_vefsida/forrituntk/base.py
+++ forrituntk_vefsida/forrituntk/base.py
@@ -61,3 +61,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
