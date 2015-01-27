edb336bda3c8cb4ebdc6081dd83c756b491d3b92
Arnór Valdimarsson <arnorv@gmail.com>
2015-01-27 02:24:31 +0000

    Önnur útfærsla á discourse tengingu

--- forrituntk_vefsida/discourse/views.py
+++ forrituntk_vefsida/discourse/views.py
@@ -1,45 +1,19 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.conf import settings
+from django.shortcuts import redirect
 
-from discourse.discoursesso import DiscourseSSO
+from discourse.sso import sso_validate, sso_redirect_url
 
 """
 Taken from https://meta.discourse.org/t/sso-example-for-django/14258
 """
 
+SECRET = settings.DISCOURSE_SSO_SECRET
 
 @login_required
 def sso(request):
-    discourse_sso = DiscourseSSO(settings.DISCOURSE_SSO_SECRET)
     payload = request.GET.get('sso')
     signature = request.GET.get('sig')
-
-    if None in [payload, signature]:
-        return HttpResponseBadRequest('No SSO payload or signature. Please contact support if this problem persists.')
-
-    # Validate the payload
-    validated = discourse_sso.validate(payload=payload, sig=signature)
-
-    if validated:
-        return HttpResponseBadRequest('Invalid payload. Please contact support if this problem persists.')
-
-    # Build the return payload
-    nounce = discourse_sso.get_nonce(payload)
-
-    if nounce == '':
-        return HttpResponseBadRequest("Nonce could not be found in payload")
-
-    params = {
-        'nonce': nounce,
-        'email': request.user.email,
-        'external_id': request.user.id,
-        'username': request.user.username,
-    }
-
-    login_url = discourse_sso.build_login_url(params)
-
-    # Redirect back to Discourse
-
-    url = '%s/session/sso_login' % settings.DISCOURSE_BASE_URL
-    return HttpResponseRedirect('%s?%s' % (url, login_url))
+    nonce = sso_validate(payload, signature, SECRET)
+    url = sso_redirect_url(nonce, SECRET, request.user.email, request.user.id, request.user.username)
+    return redirect('http://discourse.forritun.tk' + url)
